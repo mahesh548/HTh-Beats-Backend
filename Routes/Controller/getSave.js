d86bb36379cc112d35cd6c34b1b3c203437c97af
mahesh548 <mkmjnp5@gmail.com>
2025-01-09 00:03:18 +0530

    Updated: get save to add like playlist in response

--- Routes/Controller/getSave.js
+++ Routes/Controller/getSave.js
@@ -1,18 +1,25 @@
+const Entity = require("../../Database/Models/Entity");
 const Library = require("../../Database/Models/Library");
 
 const getSave = async (req, res) => {
   const { id } = req.body.user;
   try {
-    const savedData = await Library.find({ userId: { $in: [id] } }, [
+    let savedData = await Library.find({ userId: { $in: [id] } }, [
       "id",
       "type",
+      "userId",
+      "createdAt",
     ])
       .limit(10)
       .populate({
         path: "data",
         select:
-          "header_desc id image perma_url title subtitle type name fan_count",
+          "header_desc id image perma_url title subtitle type name fan_count  ",
       });
+
+    const getLiked = await Entity.findOne({ id: id, perma_url: id });
+    const liked = { id: id, type: "liked", userId: [id], data: getLiked };
+    savedData = { ...liked, ...savedData };
 
     return res.status(200).json({ status: true, data: savedData });
   } catch (error) {
