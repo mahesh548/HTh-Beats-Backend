3c3537c381809ff6d39482a39e756b730209e33e
mahesh548 <mkmjnp5@gmail.com>
2025-01-15 18:48:30 +0530

    Enhance username validation response to include field indication for invalid and existing usernames

--- Routes/Middlewares/googleMidSignup.js
+++ Routes/Middlewares/googleMidSignup.js
@@ -5,13 +5,19 @@ const googleMidSignup = async (req, res, next) => {
   const { username } = req.body;
   const validUsername = utils.isValidUsername(username);
   if (!validUsername)
-    return res.status(200).json({ status: false, msg: "Invalid Username!" });
+    return res
+      .status(200)
+      .json({ status: false, field: "username", msg: "Invalid Username!" });
 
   const user = await Users.findOne({ username });
   if (user)
     return res
       .status(200)
-      .json({ status: false, msg: "Username Already Exist!" });
+      .json({
+        status: false,
+        field: "username",
+        msg: "Username Already Exist!",
+      });
 
   req.body.username = validator.escape(username);
   next();
