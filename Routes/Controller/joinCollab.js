07802682462a21b9e0bd1186f2feb61c42d8a991
Mahesh <mkmjnp5@gmail.com>
2025-03-23 14:52:22 +0530

    Implement JWT verification in joinCollab function and enhance error handling for playlist access

--- Routes/Controller/joinCollab.js
+++ Routes/Controller/joinCollab.js
@@ -1,33 +1,62 @@
 const Entity = require("../../Database/Models/Entity");
 const Library = require("../../Database/Models/Library");
-
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 const joinCollab = async (req, res) => {
-  const { id, user } = req.body;
-  if (!id)
-    return res.status(400).json({ status: false, msg: "id is required" });
+  const { token, user } = req.body;
+  if (!validator.isJWT(token))
+    return res
+      .status(400)
+      .json({ status: false, msg: "invite token required!" });
 
   try {
-    const saveData = await Library.findOne({ id: id, type: "collab" });
+    //verify and get library secrate from jwt
+    const secrate = process.env.SECRATE;
+    const { id, librarySecrate } = jwt.verify(token, secrate);
+
+    //getting lirary with librarySecrate included
+    const verifyData = await Library.findOne({
+      id: id,
+    }).select("+librarySecrate");
+
+    //verify if librarySecrate matches secrate stored in token
+    if (!verifyData && verifyData.librarySecrate != librarySecrate)
+      return res
+        .status(200)
+        .json({ status: false, msg: "playlist unavailable!" });
+
+    //get the saved library and playlist
+    const saveData = await Library.findOne({ id: id });
     const playlistData = await Entity.findOne({ id: id });
+
     if (saveData && playlistData) {
+      //return if already user exist
       if (saveData.userId.includes(user.id)) {
-        return res.status(200).json({ status: true });
+        return res.status(200).json({ status: true, playlistId: id });
       }
+
+      //add the user
       saveData.userId = [...saveData.userId, user.id];
       playlistData.userId = [...playlistData.userId, user.id];
+
+      //save the doc
       await saveData.save();
       await playlistData.save();
-      await Activity.saveLog({
+
+      /* await Activity.saveLog({
         userId: user.id,
         activity: "joined",
         id: id,
         type: "collab",
-      });
-      return res.status(200).json({ status: true });
+      }); */
+
+      return res.status(200).json({ status: true, playlistId: id });
     }
     return res.status(400).json({ status: false, msg: "invalid id!" });
   } catch (error) {
-    return res.status(500).json({ status: false, msg: error.message });
+    return res
+      .status(200)
+      .json({ status: false, msg: "playlist unavailable!" });
   }
 };
 module.exports = joinCollab;
