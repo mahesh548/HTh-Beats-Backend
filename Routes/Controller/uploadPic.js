0e87320d00ef64f286bcfd8202c64949878646e7
Mahesh <mkmjnp5@gmail.com>
2025-04-17 14:46:32 +0530

    Enhance image upload functionality by adding user data validation and signed URL generation

--- Routes/Controller/uploadPic.js
+++ Routes/Controller/uploadPic.js
@@ -1,5 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
+const Users = require("../../Database/Models/Users");
 const uploadPic = async (req, res) => {
   const user = req.body.user;
   try {
@@ -8,21 +9,56 @@ const uploadPic = async (req, res) => {
         .status(200)
         .json({ status: false, uploaded: false, msg: "file not allowed!" });
 
-    const result = await streamUpload(req.file.buffer, user.id);
-
-    const signedUrl = cloudinary.url(result.public_id, {
-      type: "authenticated",
-      sign_url: true,
-      secure: true,
-      version: result.version,
-      expires_at: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365 * 20,
+    const userData = await Users.findOne({
+      id: user.id,
     });
+    if (!userData)
+      return res.status(400).json({ status: false, uploaded: false });
+
+    //uploading to cloudinary
+    const result = await streamUpload(req.file.buffer, user.id);
+    const publicId = result.public_id;
+    const versionCode = result.version;
+
+    if (userData.cloudinaryPublicId && publicId && versionCode) {
+      //if user already have uploaded profile pic delete that
+      try {
+        await cloudinary.uploader.destroy(userData.cloudinaryPublicId, {
+          resource_type: "image",
+          type: "authenticated",
+        });
+      } catch (err) {
+        console.error("Error deleting old image:", err.message);
+      }
+    }
+
+    if (publicId && versionCode) {
+      //generate signed url using public id
+      const signedUrl = cloudinary.url(publicId, {
+        type: "authenticated",
+        sign_url: true,
+        secure: true,
+        version: versionCode,
+        expires_at: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365 * 5,
+      });
+
+      //update the user data in DB
+      await Users.findOneAndUpdate(
+        { id: user.id },
+        {
+          $set: {
+            pic: signedUrl,
+            cloudinaryPublicId: publicId,
+            cloudinaryVersion: versionCode,
+          },
+        }
+      );
+    }
 
     res.json({
       status: true,
       uploaded: true,
       msg: "Uploaded successfully",
-      url: signedUrl,
     });
   } catch (err) {
     console.error(err);
